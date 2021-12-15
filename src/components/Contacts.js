e206ade54c85fc8538d7766b59c000e3673b0181
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-15 17:56:04 +0100

    Adding reducer and dispatch

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,16 +3,7 @@ import { Consumer } from '../context';
 import Contact from './Contact'
 
 class Contacts extends Component {
-    deleteContact = (id) => {
-        const { contacts } = this.state;
-        const newContacts = contacts.filter(contact => contact.id !== id);
-
-        this.setState({
-            contacts: newContacts
-        })
-    }
     render() {
-
         return(
             <Consumer>
                 {value => {
@@ -21,7 +12,7 @@ class Contacts extends Component {
                         <>
                             {
                                 contacts.map(contact => (
-                                <Contact key={contact.id} contact={contact} deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
+                                <Contact key={contact.id} contact={contact} />
                                 ))
                             }
                         </>
