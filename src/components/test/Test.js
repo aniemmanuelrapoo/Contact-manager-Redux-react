6efccb9d84184b3606aae8963bf321901aa02470
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-19 20:41:47 +0100

    working on fetch with jsonplace holder

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
-    componentDidMount(){
-        console.log('componeent amoutugo')
+    state = {
+        title: '',
+        completed: "",
     }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => this.setState({
+            title: data.title,
+            completed: data.completed
+        }))
+    }
+
+    // componentWillMount(){
+    //     console.log('componentWillMount!...')
+    // }
+
+    // componentDidUpdate(){
+    //     console.log('componentDidUpdate!...')
+    // }
+    // componentWillReceiveProps(nextProps, nextState){
+    //     console.log('componentWillReceiveProps!...')
+    // }
+    // static getDerivedStateFromProps(nextProps, prevState){
+    //     return {
+    //         test: 'something'
+    //     }
+    // }
+    
+    // getSnapshotBeforeUpdate(nextProps, nextState){
+    //     console.log('getSnapshotBeforeUpdate!...')
+    // }
+
     render() {
         return (
             <div>
