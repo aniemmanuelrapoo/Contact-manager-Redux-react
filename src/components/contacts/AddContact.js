eb6ce243461a3b133b1b90685946041eba7d8a7f
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-23 03:31:29 +0100

    axios post and delete request

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addContact } from '../../actions/contactAction'
 import PropTypes from 'prop-types'
-import { v4 as uuidv4 } from 'uuid';
 
 class AddContact extends Component {
     state = {
@@ -35,7 +34,6 @@ class AddContact extends Component {
         }
 
         const newContact = {
-            id: uuidv4(),
             name,
             email,
             phone
