c78a3b76ea265cb6302bfa16640374ca398ce7f2
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-23 03:10:42 +0100

    axios to get request

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,32 +1,14 @@
 import { ADD_CONTACT, DELETE_CONTACT, GET_CONTACTS } from "../actions/types";
 const initialState = {
-    contacts: [
-        {
-            id: 1,
-            name: 'Emma Rapoo',
-            email: 'rapoo@gmail.com',
-            phone: '555-555-5555'
-        },
-        {
-            id: 2,
-            name: 'Light Bro',
-            email: 'light@gmail.com',
-            phone: '555-555-5555'
-        },
-        {
-            id: 3,
-            name: 'Favour sis',
-            email: 'favpur@gmail.com',
-            phone: '555-555-5555'
-        }
-    ]
+    contacts: []
 };
 
 export default function(state = initialState, action){
     switch (action.type) {
         case GET_CONTACTS:
             return{
-                ...state
+                ...state,
+                contacts: action.payload
             }
         case DELETE_CONTACT:
             return{
